f128eba4b2343e24e6bdf01f945d4b4439cddff7
岩 <rocco.mormont@gmail.com>
2019-07-25 17:58:00 +0800

    Create import-all.js

---
+++ utils/import-all.js
@@ -0,0 +1,24 @@
+import { isFunction } from 'lodash';
+
+/**
+ * 导入一个模块上下文
+ *
+ * @export
+ * @param {*} context 模块上下文，require.context 的返回值
+ * @param {object} [options={}]
+ * @returns
+ */
+export default function importAll(context, options = {}) {
+  const { useDefault = true, keyTransformFunc, filterFunc } = options;
+  let keys = context.keys();
+
+  if (isFunction(filterFunc)) {
+    keys = keys.filter(filterFunc);
+  }
+
+  return keys.reduce((acc, curr) => {
+    const key = isFunction ? keyTransformFunc(curr) : curr;
+    acc[key] = useDefault ? context(curr).default : context(curr);
+    return acc;
+  }, {});
+}
